dc20bfd4d9a2a416f8dbd1559ca3ba1863c1deeb
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 13:29:45 -0500

    PCB render working

--- qhacks/src/App.js
+++ qhacks/src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+import axios from 'axios';
+import base64 from 'base64-img';
+
+const endpoint = "http://localhost:3000/upload"
+
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
+
 
   handleChange = name => event => {
     this.setState({
@@ -18,8 +25,37 @@ class App extends Component {
     
   };
 
+  handleUpload = () => {
+    const data = new FormData()
+    data.append('gerber', this.state.selectedFile, this.state.selectedFile.name)
+
+    axios
+      .post(endpoint, data, {
+        onUploadProgress: ProgressEvent => {
+          this.setState({
+            loaded: (ProgressEvent.loaded / ProgressEvent.total*100),
+          })
+        },
+      })
+      .then(res => {
+        console.log(res.statusText);
+        if (res.statusText == 'OK') {
+          this.setState({displayImage : true});
+        }
+      });
+
+  }
+
   handleClick = () => {
     console.log("Button clicked")
+  }
+
+  handleselectedFile = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+    console.log(event.target.files[0]);
   }
 
   render() {
@@ -79,9 +115,19 @@ class App extends Component {
           />
         </div>
         <div>
-          <p></p>
-          <Button color='primary' onClick={this.handleClick}>Compute</Button>
+          <input type="file" onChange={this.handleselectedFile} />
         </div>
+        <div>
+          <p></p>
+          <Button color='primary' onClick={this.handleUpload}>Compute</Button>
+        </div>
+        {
+          this.state.displayImage ? 
+          <div>
+            <img src="http://localhost:3000/output.png"/>
+          </div>
+        : null
+        }
       </div>
     );
   }
