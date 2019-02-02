19f8ffe7db69825d0195c8c895d0eac50f66b41e
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 11:23:41 -0500

    setup simple webpage

--- qhacks/src/App.js
+++ qhacks/src/App.js
@@ -2,24 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TextField from '@material-ui/core/TextField';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          </p>*/}
+          <p>Welcome to <code>CircuitSafe</code>✔</p>
         </header>
+        <TextField
+          id="min_width"
+          label="Minimum PCB Trace Width"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
       </div>
     );
   }
---
+++ qhacks/src/entry.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+
+// type Props = {};
+// export default class Entry extends Component<Props> {
+//     props: Props;
+//     constructor(props) {
+//         super(props)
+//         this.state = {}
+//     }
+
+//     render() {
+//         return (
+//             <div>TEST</div>
+//         )
+//     }
+// }
