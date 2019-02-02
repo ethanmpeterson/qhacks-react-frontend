3015ce26afa193394b6fed03fab91ef7c8dcab0f
Ethan Peterson <17emp5@queensu.ca>
2019-02-02 11:48:59 -0500

    added button click and textfield data collection callbacks

--- qhacks/src/App.js
+++ qhacks/src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+    
+  };
+
+  handleClick = () => {
+    console.log("Button clicked")
   }
 
   render() {
@@ -20,15 +32,56 @@ class App extends Component {
           </p>*/}
           <p>Welcome to <code>CircuitSafe</code>✔</p>
         </header>
-        <TextField
-          id="min_width"
-          label="Minimum PCB Trace Width"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          margin="normal"
-        />
+        <div>
+          <p>Measurements mil</p>
+          <p>Voltage in Volts and Current is given in Amperes</p>
+        </div>
+        <div>
+          <TextField
+            id="min_width"
+            label="Minimum PCB Trace Width"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={this.handleChange('min_width')}
+            margin="normal"
+          />
+          <TextField
+            id="min_dist"
+            label="Minimum Distance Between Traces"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={this.handleChange('min_dist')}
+            margin="normal"
+          />
+          <TextField
+            id="voltage"
+            label="Circuit Voltage Specification"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={this.handleChange('voltage')}
+            margin="normal"
+          />
+          <TextField
+            id="current"
+            label="Circuit Amperage (A)"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={this.handleChange('current')}
+            margin="normal"
+          />
+        </div>
+        <div>
+          <p></p>
+          <Button color='primary' onClick={this.handleClick}>Compute</Button>
+        </div>
       </div>
     );
   }
